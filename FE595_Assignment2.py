dd50374f3569120cf44198066e1270097c789229
VaikunthSeshadri <66288404+VaikunthSeshadri@users.noreply.github.com>
2020-10-08 10:47:22 -0400

    Create FE595_Assignment2.py

---
+++ FE595_Assignment2.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+f = open("C:/Users/vaiku/FE 595 Files/Assignments/Results2.txt", "w")
+for j in range(50):
+    r = requests.get('http://3.95.249.159:8000/random_company')
+    alltext = r.text
+    words = alltext.split('\n')
+
+    for i in words:
+        if 'Name:' in i:
+            namestr = i
+            namestr = namestr[16:-5]
+            f.write(namestr + '\n')
+        if 'Purpose' in i:
+            allpurpose = i
+            allpurpose = allpurpose[16:-5]
+            f.write(allpurpose + '\n')
+
+f.close()
